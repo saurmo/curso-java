2db57204e49f85cff62e1825838676a1e13f4c6a
s109e19 <saurmo0108@gmail.com>
2019-09-07 11:57:34 -0500

    Poo

---
+++ src/poo/EjercicioPOO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author s109e19
+ */
+public class EjercicioPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Perro miPerro;
+        miPerro = new Perro();
+
+        Perro miPerro2 = new Perro("firu");
+
+        Perro miPerro3 = new Perro("firu", "pincher", 50.0);
+    }
+
+}
---
+++ src/poo/Perro.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author s109e19
+ */
+public class Perro {
+
+    private String nombre;
+    private String raza;
+    private double altura;
+
+    //SOBRECARGA DE METODOS
+    public Perro() {
+
+    }
+
+    public Perro(String nombre) {
+
+    }
+
+    public Perro(String nombre, String raza, double altura) {
+
+    }
+
+}
